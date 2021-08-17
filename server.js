1893d1ec43b37b4602cc2471fdf1c7cfafb6fe12
jorge <jorge.del@hotmail.com>
2021-08-17 14:40:59 -0400

    u

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/auth-vazul-ui'));
 
-app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/src/index.html'));
-});
+// app.get('/*', (req,res,next) => {
+//     res.sendFile(path.join(__dirname + '/dist/src/index.html'));
+// });
 
 // Another way to do it 
 // app.get('/*', (req,res,next) => {
 //     res.sendFile(path.join('index.html', __dirname + 'dist/auth-vazul-ui/'));
 // });
+
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'src', 'index.html')
+    res.sendFile(index);
+})
 
 app.listen(process.env.PORT || 8000);
