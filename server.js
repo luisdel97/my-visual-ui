8119dc96cd875f16719408b1bb50555c4ffd7d42
jorge <jorge.del@hotmail.com>
2021-08-17 14:15:01 -0400

    rem

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/auth-vazul-ui'));
+app.use(express.static(__dirname + '/auth-vazul-ui'));
 
 app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/src/index.html'));
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 // Another way to do it 
