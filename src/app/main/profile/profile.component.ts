cfa36a7f51693e9df905187d63f3d69f58bdfcd9
jorge <jorge.del@hotmail.com>
2021-08-17 12:58:30 -0400

    updated name

--- src/app/main/profile/profile.component.ts
+++ src/app/main/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { ApiService } from 'src/app/services/api.service';
+import { UserInfo } from 'src/app/models/user/user';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  userInfos: any = [];
+  constructor(
+    private _api : ApiService,
+    private _auth: AuthService,
+  ) { }
 
   ngOnInit(): void {
+    this.getUser();
   }
+
+  getUser() {
+    this._api.getTypeRequest('/profile').subscribe((data: {}) => {
+      this.userInfos = data;
+
+    });
+  }
+
+  // getComment() {
+  //   return this._api.getTypeRequestComment('/comments').subscribe((data: {}) => {
+  //     this.commentInfos = data;
+  //   })
+
+  // }
 
 }
