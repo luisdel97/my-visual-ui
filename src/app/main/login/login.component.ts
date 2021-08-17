45f45d5e7acc4377c85841831ca0c20af8fd5bd1
jorge <jorge.del@hotmail.com>
2021-08-17 11:18:52 -0400

    test

--- src/app/main/login/login.component.ts
+++ src/app/main/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import {ApiService} from '../../services/api.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  constructor(
+    private _api : ApiService,
+    private _auth: AuthService,
+    private router: Router,
+    public fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.email],
+      password: ['', Validators.required]
+    });
+
   }
+
+  login() {
+    const b = this.form.value;
+    console.log(b);
+    this._api.postTypeRequest('/login', b).subscribe((res: any) => {
+      console.log(res);
+      if (res.token) {
+        this._auth.setDataInLocalStorage('token', res.token);
+        this.router.navigate(['/profile']);
+
+      }
+    }, err => {
+      console.log(err);
+    });
+  }
+
 
 }
